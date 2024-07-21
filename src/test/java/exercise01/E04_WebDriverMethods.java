e31381cd8ebaa46886876820e1f36d9027375980
rananurulker <rananurulker@gmail.com>
2024-07-21 23:38:41 +0300

    new

---
+++ src/test/java/exercise01/E04_WebDriverMethods.java
@@ -0,0 +1,33 @@
+package exercise01;
+
+import Utilities.TestBaseAll;
+import org.junit.jupiter.api.Test;
+
+public class E04_WebDriverMethods extends TestBaseAll {
+    @Test
+    public void test01(){
+
+        //1.Yeni bir class olusturalim (Homework)
+
+        //2.ChromeDriver kullanarak, facebook sayfasina gidin ve sayfa basliginin
+
+        //(title) “facebook” oldugunu test edin, degilse dogru basligi yazdirin.
+
+        //3.Sayfa URL’inin “facebook” kelimesi icerdigini dogrulayin, icermiyorsa
+
+        //“actual” URL’i yazdirin.
+
+        //4.https://www.walmart.com/ sayfasina gidin.
+
+        //5. Sayfa basliginin “Walmart.com” icerdigini dogrulayin.
+
+        //6. Tekrar “facebook” sayfasina donun
+
+        //7. Sayfayi yenileyin
+
+        //8. Sayfayi tam sayfa (maximize) yapin
+
+        //9.Browser’i kapatin
+    }
+
+}
---
+++ src/test/java/exercise01/E05_WebDriverMethods.java
@@ -0,0 +1,41 @@
+package exercise01;
+
+import Utilities.TestBaseAll;
+import org.junit.jupiter.api.Test;
+
+public class E05_WebDriverMethods extends TestBaseAll {
+    @Test
+    public void test01(){
+        //1. Yeni bir class olusturun (TekrarTesti)
+
+        //2. Youtube web sayfasına gidin ve sayfa başlığının “youtube” olup olmadığını
+
+        //doğrulayın (verify), eğer değilse doğru başlığı(Actual Title) konsolda yazdirin.
+
+        //3. Sayfa URL'sinin “youtube” içerip içermediğini (contains) doğrulayın, içermiyorsa
+
+        //doğru URL'yi yazdırın.
+
+        //4. Daha sonra testotomasyonu sayfasina gidin https://www.testotomasyonu.com/
+
+        //5. Youtube sayfasina geri donun
+
+        //6. Sayfayi yenileyin
+
+        //7. Testotomasyonu sayfasina donun
+
+        //8. Sayfayi tamsayfa yapin
+
+        //9. Ardından sayfa başlığının "Test" içerip içermediğini (contains) test edin, Yoksa
+
+        //doğru başlığı(Actual Title) yazdırın.
+
+        //10.Sayfa URL'sinin https://www.testotomasyonu.com/ olup olmadığını test edin,
+
+        //degilse doğru URL'yi yazdırın
+
+        //11.Sayfayi kapatin
+
+
+    }
+}
